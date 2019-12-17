6008317c9842634dc66b6f36ad35a0b548a1545e
edomond2414 <edomond2414@gmail.com>
2019-12-17 20:33:43 +0900

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,7 @@ module ChatSpace
       g.stylesheets false
       g.javascripts false
       g.helper false
-      g.test_framework false #テスト
-      #tesuto
+      g.test_framework false 
     end   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
